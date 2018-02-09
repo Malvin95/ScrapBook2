c485a560dca79106b3b5848b27cf961330af8134
Malvin <Malvinharding@hotmail.co.uk>
2018-02-09 17:12:35 +0000

    Null pointer Issue sorted out!!

--- app/src/main/java/com/mobile/apex/scrapbook21/Fragments/HolidayFragment.java
+++ app/src/main/java/com/mobile/apex/scrapbook21/Fragments/HolidayFragment.java
@@ -72,7 +72,7 @@ public class HolidayFragment extends Fragment {
             } else {
                 recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
             }
-            recyclerView.setAdapter(new MyHolidayRecyclerViewAdapter(/**Holiday.ITEMS*/ HolidayData.getInstance().getAllHolidays(), mListener));
+            recyclerView.setAdapter(new MyHolidayRecyclerViewAdapter(HolidayData.getInstance(getActivity().getBaseContext()).getAllHolidays(), mListener));
         }
         return view;
     }
