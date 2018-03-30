0ed30e2608b80273a40d95d070332947cd3add2a
Malvin <malvinharding@hotmail.co.uk>
2018-03-30 23:55:07 +0100

    Working, but not quite as wanted. The Bottom sheet seems to come, but only as part of another activity being opened within the google Maps application.
    FOCUS:
    Look at a way to implement the bottomsheets without opening a new activity.

--- app/src/main/java/com/mobile/apex/scrapbook21/MapsActivity.java
+++ app/src/main/java/com/mobile/apex/scrapbook21/MapsActivity.java
@@ -1,10 +1,12 @@
 package com.mobile.apex.scrapbook21;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -15,26 +17,38 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
 import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.mobile.apex.scrapbook21.Adapters.CustomInfoWindowAdapter;
 import com.mobile.apex.scrapbook21.Adapters.PlacesAutoCompleteAdapter;
+import com.mobile.apex.scrapbook21.model.PlaceInfo;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -51,25 +65,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final float DEFAULT_ZOOM = 15f;
     private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(
             new LatLng(-50, -198), new LatLng(56, 154));
-
-    private PlacesAutoCompleteAdapter mPlacesAutoCompleteAdapter;
-    private GoogleApiClient mGoogleApiClient;
-
-    private Boolean mLocationPermissionsGranted = false;
-    private FusedLocationProviderClient mFusedLocationProviderClient;
-    private GoogleMap mMap;
+    private int PLACE_PICKER_REQUEST = 1;
 
     //widgets
     private AutoCompleteTextView mSearchText;
-    private ImageView mGps;
+    private ImageView mGps, mInfo, mPlacePicker;
+
+    //variables
+    private Boolean mLocationPermissionsGranted = false;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private PlacesAutoCompleteAdapter mPlacesAutoCompleteAdapter;
+    private GoogleApiClient mGoogleApiClient;
+    private GoogleMap mMap;
+    private PlaceInfo mPlace;
+    private Marker mMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         mSearchText = (AutoCompleteTextView) findViewById(R.id.input_search);
-
         mGps = (ImageView) findViewById(R.id.ic_LocationGps);
+        mInfo = (ImageView)findViewById(R.id.place_info);
+        mPlacePicker = (ImageView)findViewById(R.id.place_picker);
 
         getLocationPermission();
 
@@ -81,17 +99,60 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         init();
     }
 
+    private void moveCamera(LatLng latLng, float zoom, PlaceInfo placeInfo)
+    {
+        Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude );
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
+
+        mMap.clear();
+
+        mMap.setInfoWindowAdapter(new CustomInfoWindowAdapter(MapsActivity.this));
+
+        if(placeInfo != null)
+        {
+
+            try
+            {
+                    String snippet = "Address: " + placeInfo.getAddress() + "\n" +
+                            "Phone Number: " + placeInfo.getPhoneNumber() + "\n" +
+                            "Website: " + placeInfo.getWebsiteUri() + "\n" +
+                            "Price Rating: " + placeInfo.getRating() + "\n";
+
+                    MarkerOptions options = new MarkerOptions()
+                            .position(latLng)
+                            .title(placeInfo.getName())
+                            .snippet(snippet);
+
+                    mMarker = mMap.addMarker(options);
+
+                }
+                catch(NullPointerException e)
+                {
+                    Log.e(TAG,"moveCamera: NullPointerException: " + e.getMessage() );
+                }
+
+            hideSoftKeyboard();
+        }
+        else
+        {
+            mMap.addMarker(new MarkerOptions().position(latLng));
+        }
+    }
+
     private void moveCamera(LatLng latLng, float zoom, String title)
     {
         Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude );
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
 
+        if(!title.equals("My Location")){
         MarkerOptions options = new MarkerOptions()
                 .position(latLng)
                 .title(title);
 
         mMap.addMarker(options);
+        }
 
+        hideSoftKeyboard();
     }
 
     private void init(){
@@ -104,6 +165,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .enableAutoManage(this, this)
                 .build();
 
+        mSearchText.setOnItemClickListener(mAutocompleteClickListener);
         mPlacesAutoCompleteAdapter = new PlacesAutoCompleteAdapter(this, mGoogleApiClient,
                 LAT_LNG_BOUNDS, null);
 
@@ -133,7 +195,61 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
+        mInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked place info");
+                try
+                {
+                    if(mMarker.isInfoWindowShown())
+                    {
+                        mMarker.hideInfoWindow();
+                    }
+                    else
+                    {
+                        Log.d(TAG, "onCLick: place info: " + mPlace.toString());
+                        mMarker.showInfoWindow();
+                    }
+                }
+                catch (NullPointerException e)
+                {
+                    Log.e(TAG, "onClick: NullPointerException: " + e.getMessage() );
+                }
+            }
+        });
+
+        mPlacePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PlacePicker.IntentBuilder builder= new PlacePicker.IntentBuilder();
+
+                try {
+                    startActivityForResult(builder.build(MapsActivity.this), PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException e) {
+                    Log.e(TAG, "onClick: GooglePlayServicesRepairableException: " + e.getMessage());
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    Log.e(TAG, "onClick: GooglePlayServicesNotAvailableException: " + e.getMessage());
+                }
+            }
+        });
+
         hideSoftKeyboard();
+    }
+
+    protected void  onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == PLACE_PICKER_REQUEST)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                Place place = PlacePicker.getPlace(this, data);
+
+
+                PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
+                        .getPlaceById(mGoogleApiClient, place.getId());
+                placeResult.setResultCallback(mUpdatePlaceDetailsCallback);
+            }
+        }
     }
 
 
@@ -293,4 +409,61 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
+
+    /*
+        Google Places API autocomplete suggestions
+     */
+
+    private AdapterView.OnItemClickListener mAutocompleteClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            hideSoftKeyboard();
+
+            final AutocompletePrediction item = mPlacesAutoCompleteAdapter.getItem(position);
+            final String placeId = item.getPlaceId();
+
+            PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
+                    .getPlaceById(mGoogleApiClient, placeId);
+            placeResult.setResultCallback(mUpdatePlaceDetailsCallback);
+        }
+    };
+
+    private ResultCallback<PlaceBuffer> mUpdatePlaceDetailsCallback = new ResultCallback<PlaceBuffer>() {
+        @Override
+        public void onResult(@NonNull PlaceBuffer places) {
+            if(!places.getStatus().isSuccess())
+            {
+                Log.d(TAG, "onResult: Place query did not complete successfully: " + places.getStatus().toString());
+                places.release();
+                return;
+            }
+
+            final Place place = places.get(0);
+
+            try
+            {
+                mPlace = new PlaceInfo();
+                mPlace.setName(place.getName().toString());
+                mPlace.setAddress(place.getAddress().toString());
+                //mPlace.setAttributions(place.getAttributions().toString());
+                mPlace.setPhoneNumber(place.getPhoneNumber().toString());
+                mPlace.setWebsiteUri(place.getWebsiteUri());
+                mPlace.setId(place.getId());
+                mPlace.setLatlng(place.getLatLng());
+                mPlace.setRating(place.getRating());
+
+                Log.d(TAG, "onReuslt: place: " + mPlace.toString());
+
+            }
+            catch(NullPointerException e)
+            {
+                Log.e(TAG, "onResult: NullPointerException: " + e.getMessage() );
+            }
+
+            moveCamera(new LatLng(place.getViewport().getCenter().latitude,
+                    place.getViewport().getCenter().longitude), DEFAULT_ZOOM, mPlace);
+
+            places.release();
+        }
+    };
 }
